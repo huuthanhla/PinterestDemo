0f06ffe414e5b9536f8b557a1ebe08013ee50470
thanhlazio <thanhlh@lifetimetech.vn>
2018-09-01 23:00:01 +0700

    replace proj

---
+++ PinterestDemo/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  PinterestDemo
+//
+//  Created by MINATO on 9/1/18.
+//  Copyright © 2018 MINATO. All rights reserved.
+//
+
+import UIKit
+
+class PhotoStreamViewController: UICollectionViewController {
+    
+    var photos = Photo.allPhotos()
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let patternImage = UIImage(named: "Pattern") {
+            view.backgroundColor = UIColor(patternImage: patternImage)
+        }
+        collectionView?.backgroundColor = UIColor.clear
+        collectionView?.contentInset = UIEdgeInsets(top: 23, left: 10, bottom: 10, right: 10)
+        // Set the PinterestLayout delegate
+        if let layout = collectionView?.collectionViewLayout as? PinterestLayout {
+            layout.delegate = self
+        }
+    }
+    
+}
+
+extension PhotoStreamViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnnotatedPhotoCell", for: indexPath)
+        if let annotateCell = cell as? AnnotatedPhotoCell {
+            annotateCell.photo = photos[indexPath.item]
+        }
+        return cell
+    }
+    
+}
+
+//MARK: - PINTEREST LAYOUT DELEGATE
+extension PhotoStreamViewController : PinterestLayoutDelegate {
+    
+    // 1. Returns the photo height
+    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath:IndexPath) -> CGFloat {
+        return photos[indexPath.item].image.size.height
+    }
+    
+}
